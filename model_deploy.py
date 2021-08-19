cd782dbc2456d3595dbfab4b3541424f16d01f95
Zhilei Ma <80358105+zmaalgorithmia@users.noreply.github.com>
2021-08-19 18:23:09 -0400

    Updated deployment workflow

--- model_deploy.py
+++ model_deploy.py
@@ -7,15 +7,17 @@ from six.moves.urllib.parse import quote_plus
 from tempfile import mkdtemp
 from time import sleep
 
-# CONFIGURABLE SETTINGS:
+# --------- Algorithm name and metatadata ---------
+#
 
-# using a rotating algo name for demo, but this can be any name you prefer; it will be created under https://{ALGORITHMIA_ENDPOINT}/algorithms/[YOUR_USERNAME]
-ALGO_NAME = 'digit_recognition_'+datetime.now().strftime('%Y%m%d%H%M%S')
+# The algorithm will be created under https://{ALGORITHMIA_ENDPOINT}/algorithms/[YOUR_USERNAME]
+ALGO_NAME = 'digit_recognition'
 
-# pick any collection name you prefer; it will be created for you in https://{ALGORITHMIA_ENDPOINT}/data/hosted/
-COLLECTION_NAME = 'digit_recognition'
+# The data collection will be created at https://{ALGORITHMIA_ENDPOINT}/data/{COLLECTION_NAME}
+# We use the algoritm name as the collection name here, but it could be any legal name
+COLLECTION_NAME = f"{ALGO_NAME}"
 
-# config your Algorithm details/settings as per https://docs.algorithmia.com/#create-an-algorithm
+# Config your algorithm details/settings as per https://docs.algorithmia.com/#create-an-algorithm
 ALGORITHM_DETAILS = {
     'label': 'Digit Recognition',
     'tagline': 'Digit recognition from an image file'
@@ -47,16 +49,22 @@ def UPDATE_ALGORITHM_TEMPLATE(file_contents):
     return file_contents.replace('data://username/demo/'+MODEL_FILE, data_path+'/'+MODEL_FILE)
 
 
-# DEPLOYMENT SCRIPT:
-
-# verify that environment keys are set
+# --------- REQUIRED ENVIRONMENT VARIABLES ----------
+#
+# ALGORITHMIA_API_KEY - must have permission to manage algorithms
+#
+# ALGORITHMIA_DOMAIN - your Algorithmia Enterprise domain
+#    E.g.: YOUR_COMPANY.productionize.ai
+#
+# ALGORITHMIA_USERNAME - self explanatory
+#
 api_key = environ.get('ALGORITHMIA_API_KEY')
 algo_domain = environ.get('ALGORITHMIA_DOMAIN')
 algo_endpoint = f"https://{algo_domain}"
 username = environ.get('ALGORITHMIA_USERNAME')
 if not api_key:
     raise SystemExit(
-        'Please set the environment variable ALGORITHMIA_MANAGEMENT_API_KEY (key must have permission to manage algorithms)')
+        'Please set the environment variable ALGORITHMIA_API_KEY (key must have permission to manage algorithms)')
 if not algo_domain:
     raise SystemExit(
         'Please set the environment variable ALGORITHMIA_DOMAIN (e.g. algorithmia.com')
@@ -82,13 +90,23 @@ print('UPLOADING model to '+data_path+'/'+MODEL_FILE)
 client.file(data_path+'/'+MODEL_FILE).putFile(ALGO_TEMPLATE_PATH+MODEL_FILE)
 
 # create the Algorithm
-print(f"CREATING algorithm: {algo_full_name}")
-print(f"ON Algorithmia cluster: {algo_endpoint}")
 try:
+    print(f"CREATING algorithm: {algo_full_name}")
+    print(f"ON Algorithmia cluster: {algo_endpoint}")
     print(algo.create(details=ALGORITHM_DETAILS, settings=ALGORITHM_SETTINGS))
 except Exception as x:
-    raise SystemExit(
-        'ERROR: cannot create {}: if the Algorithm already exists and you wish to overwrite it, remove/ignore this step\n{}'.format(algo_full_name, x))
+    print(str(x))
+    if 'already exists' in str(x):
+        try:
+            print(f"UPDATING algorithm: {algo_full_name}")
+            print(algo.update(details=ALGORITHM_DETAILS,
+                  settings=ALGORITHM_SETTINGS))
+        except Exception as x:
+            raise SystemExit(
+                'ERROR: could not UPDATE {}: \n{}'.format(algo_full_name, x))
+    else:
+        raise SystemExit(
+            'ERROR: could not CREATE {}: \n{}'.format(algo_full_name, x))
 
 # git clone the created algorithm's repo into a temp directory
 tmpdir = mkdtemp()
