0e9d519a4696242eaa7750436eaa74896bd3b677
Zhilei Ma <80358105+zmaalgorithmia@users.noreply.github.com>
2021-08-22 15:28:45 -0400

    Updated sample input

--- model_deploy.py
+++ model_deploy.py
@@ -34,7 +34,37 @@ ALGORITHM_SETTINGS = {
 # config your publish settings as per https://docs.algorithmia.com/#publish-an-algorithm
 ALGORITHM_VERSION_INFO = {
     "release_notes": "Automatically created, deployed and published from Jenkins.",
-    "sample_input": "data://algorithmia_se/CreditCardApproval/features-b.png",
+    "sample_input": {
+        "high_balance": 0,
+        "owns_home": 1,
+        "child_one": 0,
+        "child_two_plus": 0,
+        "has_work_phone": 0,
+        "age_high": 0,
+        "age_highest": 1,
+        "age_low": 0,
+        "age_lowest": 0,
+        "employment_duration_high": 0,
+        "employment_duration_highest": 0,
+        "employment_duration_low": 0,
+        "employment_duration_medium": 0,
+        "occupation_hightech": 0,
+        "occupation_office": 1,
+        "family_size_one": 1,
+        "family_size_three_plus": 0,
+        "housing_coop_apartment": 0,
+        "housing_municipal_apartment": 0,
+        "housing_office_apartment": 0,
+        "housing_rented_apartment": 0,
+        "housing_with_parents": 0,
+        "education_higher_education": 0,
+        "education_incomplete_higher": 0,
+        "education_lower_secondary": 0,
+        "marital_civil_marriage": 0,
+        "marital_separated": 0,
+        "marital_single_not_married": 1,
+        "marital_widow": 0
+    },
     "version_type": "minor"
 }
 
